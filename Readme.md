077071cc722613143256e231e3c92e695d77a92f
Timpan42 <77719322+Timpan42@users.noreply.github.com>
2023-02-09 13:33:58 +0100

    Har börjat med Gui, server och dokumentation:]

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Logbok
+
+##  09 - 02 - 2023
+Jag har börjat skapa strukturen för mcv för server och client. Jag har gjort ett enkelt GUI och börjat med att skapa något som ser ut som en 
+server 
--- src/server/model/s_Model.java
+++ src/server/model/s_Model.java
@@ -1,4 +1,27 @@
 package server.model;
 
+import com.sun.corba.se.spi.activation.Server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class s_Model {
+    ServerSocket server;
+    Socket client;
+
+    public s_Model (int port){
+        try {
+            server = new ServerSocket(port);
+        } catch (IOException e){
+            System.err.println("nämen det funkar inte, gör bättre nästa gång");
+            e.printStackTrace();
+        }
+        System.out.println("Makaroner med server, gott");
+    }
+
+    public static void main(String[] args) {
+        int port = 3;
+        s_Model s = new s_Model(port);
+    }
 }
